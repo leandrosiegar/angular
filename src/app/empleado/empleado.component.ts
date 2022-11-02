13311c45be95aa39c95e41d1dbc663ae5c63f33c
unknown <leandrosiegar@gmail.com>
2022-11-02 10:52:30 +0100

    ejemplos binding

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -19,6 +19,27 @@ export class EmpleadoComponent implements OnInit {
   }
   */
 
+  statusInputText = false;
+  userLogged = false;
+  txtLogueado = "No te has logueado todavía";
+
+  activarUser() {
+    this.userLogged = false;
+  }
+
+  showUserChecked(event:Event) {
+    // alert("El usuario ha pulsado el checkbox");
+    // this.txtLogueado = "Te has logueado correctamente";
+    // alert(event.target);
+    // para saber que tipo de elemento es (por ejemplo: HTMLInputElement) con poner console.log(event.target) ya te dije cuál es
+    if ((<HTMLInputElement>event.target).value == "S") {
+       this.txtLogueado = "Te has logueado correctamente";
+    }
+    else {
+      this.txtLogueado = "Te has deslogueado";
+    }
+  }
+
 
 
   constructor() { }
